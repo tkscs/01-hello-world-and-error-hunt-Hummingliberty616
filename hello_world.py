bc084b8843bd980696cb997c3be739d5c6d26aa0
Hummingliberty616 <wlin2028@kehillahstudent.org>
2025-08-28 09:19:59 -0700

    Found errors

---
+++ hello_world.py
@@ -0,0 +1,35 @@
+print (19%5)    
+
+#Error hunt
+    #Syntax (grammar) errors:
+        #SyntaxError: unterminated string literal (detected at line 5)
+            #The opposite of "literal" is "abstract".  
+        #SyntaxError: '(' was never closed
+        #SyntaxError: unmatched ')'
+        #SyntaxError: invalid syntax
+    #Name errors:
+        #NameError: name 'prnt' is not defined. Did you mean: 'print'?
+        #NameError: name 'Print' is not defined. Did you mean: 'print'? 
+            #Function names are case sensitive.
+    #Type errors: 
+        #TypeError: unsupported operand type(s) for -: 'builtin_function_or_method' and 'str'
+            #What is an operand?
+        #TypeError: 'NoneType' object is not callable 
+            #What does "callable" mean?
+    #Mathematical errors:
+        #ZeroDivisionError: division by zero
+    #A colon can be put between the function and the parentheses without resulting in an error.
+
+#Data types "parts of speech"
+    #Strings - They are a sequence of characters. Spaces and emojis count as characters. 
+        #Strings are marked with quotation marks.
+    #Integers - Integers are numbers with no fractional or decimal components.
+    #Floats - Floats are numbers with decimal points.
+    #Booleans (also known as logicals) - They represent the values "true" or "false".
+
+#Mathematical operators in Python
+    #Multiplication is represented by the star above the eight on the keyboard.
+    #Division is represented by a slash. Putting a slash between two numbers will truncate the result of dividing the numbers.
+    #A percent sign gives us the remainder of dividing two numbers.
+#Vocabulary 
+    #Truncate - To truncate a number is to remove its demical component.  
